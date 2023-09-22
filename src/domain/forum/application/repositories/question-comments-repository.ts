b048c4e0096a5b9a587941c941b28d608c368483
Nathan <nathanferreira00@hotmail.com>
2023-09-22 15:50:03 -0300

    feat: questions controllers

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,12 +1,13 @@
 import { QuestionComment } from '../../enterprise/entities/question-comment'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 
-export interface QuestionCommentsRepository {
-  findById(id: string): Promise<QuestionComment | null>
-  findManyByQuestionId(
+export abstract class QuestionCommentsRepository {
+  abstract findById(id: string): Promise<QuestionComment | null>
+  abstract findManyByQuestionId(
     questionId: string,
     { page }: PaginationParams,
   ): Promise<QuestionComment[]>
-  create(questionComment: QuestionComment): Promise<void>
-  delete(questionComment: QuestionComment): Promise<void>
+
+  abstract create(questionComment: QuestionComment): Promise<void>
+  abstract delete(questionComment: QuestionComment): Promise<void>
 }
---
+++ src/infra/http/controllers/fetch-question-comments.controller.ts
@@ -0,0 +1,46 @@
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common'
+import { z } from 'zod'
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe'
+import { FetchQuestionCommentsUseCase } from '@/domain/forum/application/use-cases/fetch-question-comments'
+import { CommentPresenter } from '../presenters/comment-presenter'
+
+const pageQueryParamSchema = z
+  .string()
+  .optional()
+  .default('1')
+  .transform(Number)
+  .pipe(z.number().min(1))
+
+type PageQueryParamSchema = z.infer<typeof pageQueryParamSchema>
+
+const queryValidationPipe = new ZodValidationPipe(pageQueryParamSchema)
+
+@Controller('/questions/:questionId/comments')
+export class FetchQuestionCommentsController {
+  constructor(
+    private fetchQuestionCommentsUseCase: FetchQuestionCommentsUseCase,
+  ) {}
+
+  @Get()
+  async handle(
+    @Param('page', queryValidationPipe) page: PageQueryParamSchema,
+    @Param('questionId') questionId: string,
+  ) {
+    const result = await this.fetchQuestionCommentsUseCase.execute({
+      page,
+      questionId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+
+    const { questionComments } = result.value
+
+    return {
+      questionComments: questionComments.map((questionComment) =>
+        CommentPresenter.toHTTP(questionComment),
+      ),
+    }
+  }
+}
