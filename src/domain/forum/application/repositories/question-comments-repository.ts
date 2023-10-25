68817f203a1b695fc95a495d95acc8cc07ffc703
Nathan <nathanferreira00@hotmail.com>
2023-10-25 16:42:53 -0300

    feat: fetching comments with author

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,5 +1,6 @@
 import { QuestionComment } from '../../enterprise/entities/question-comment'
 import { PaginationParams } from '@/core/repositories/pagination-params'
+import { CommentWithAuthor } from '../../enterprise/entities/value-objects/comment-with-author'
 
 export abstract class QuestionCommentsRepository {
   abstract findById(id: string): Promise<QuestionComment | null>
@@ -7,6 +8,11 @@ export abstract class QuestionCommentsRepository {
     questionId: string,
     { page }: PaginationParams,
   ): Promise<QuestionComment[]>
+
+  abstract findManyByQuestionIdWithAuthor(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<CommentWithAuthor[]>
 
   abstract create(questionComment: QuestionComment): Promise<void>
   abstract delete(questionComment: QuestionComment): Promise<void>
